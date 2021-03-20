c7f4cef75056847fba0615be87c5555b45f47787
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-03-20 13:55:24 +0100

    Implemented basic board display

---
+++ source/display/console.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include "console.h"
+#include "../board/board.h"
+
+void displayBoard(){
+    int size = getBoardSize();
+
+    printf("+");
+    for(int i = 0; i < size; i++)
+        printf("-");
+    printf("+\n");
+
+    for(int row = 0; row < size; row++){
+        printf("|");
+        for(int col = 0; col < size; col++){
+            Pawn* p = getPawnAt(row, col);
+
+            if(p == NULL){
+                // Game takes place on some fields only (black ones)
+                printf("%c", isPlayableField(row, col) ? '.' : ' ');
+                continue;
+            }
+
+            if(getPawnColor(p) == black){
+                printf("%c", isPawnKing(p) ? 'B' : 'b');
+            }else{
+                printf("%c", isPawnKing(p) ? 'W' : 'w');
+            }
+        }
+        printf("|\n");
+    }
+
+    printf("+");
+    for (int i = 0; i < size; i++)
+        printf("-");
+    printf("+\n");
+}
