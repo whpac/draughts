a52d722b493557f14a6fc3f0093ebab8db9c3915
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-03-26 12:35:36 +0100

    Added a stack implementation

---
+++ source/data/stack.h
@@ -0,0 +1,12 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct Stack Stack;
+
+Stack* stackCreate();
+void stackDestroy(Stack* s);
+char stackIsEmpty(Stack* s);
+void stackPush(Stack* s, void* e);
+void* stackPop(Stack* s);
+
+#endif
